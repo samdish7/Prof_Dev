70037b70210726d76df652c6ea942edb0b902728
samdish7 <samdish98@gmail.com>
2022-12-07 15:56:53 -0500

    dat 3 done

---
+++ COMP3810/Exercises/routingProject/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {path: 'contact', component: ContactComponent},
+    {path: 'about', component: AboutComponent},
+    {path: 'home', component: HomeComponent},
+    {path: '**', component: HomeComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
